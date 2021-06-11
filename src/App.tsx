8849bde2b0ee60bbfc235a99de932f9ab29a1b48
Junnosuke Tasaka <junnosuke0120@gmail.com>
2021-06-11 23:57:02 +0900

    feat: change indent

--- src/App.tsx
+++ src/App.tsx
@@ -27,58 +27,58 @@ const App = () => {
       <label>Length</label>
       <div>
         <label>Password Length</label>
-          <input
-            defaultValue={passwordLength}
-            type="number"
-            onChange={(e) => setPasswordLength(e.target.value)}
-            min='10'
-            max='30'
-          />
+        <input
+          defaultValue={passwordLength}
+          type="number"
+          onChange={(e) => setPasswordLength(e.target.value)}
+          min='10'
+          max='30'
+        />
       </div>
 
       <div className="check_lb">
         <label>Password Length</label>
-          <input
-            defaultChecked={includeUpperCase}
-            type="checkbox"
-            onChange={(e) => setIncludeUpperCase(e.target.checked)}
-          />
+        <input
+          defaultChecked={includeUpperCase}
+          type="checkbox"
+          onChange={(e) => setIncludeUpperCase(e.target.checked)}
+        />
       </div>
 
       <div className="check_lb">
         <label>Include Uppercase Letters</label>
-          <input
-            defaultChecked={includeLowerCase}
-            type="checkbox"
-            onChange={(e) => setIncludeLowerCase(e.target.checked)}
-          />
+        <input
+          defaultChecked={includeLowerCase}
+          type="checkbox"
+          onChange={(e) => setIncludeLowerCase(e.target.checked)}
+        />
       </div>
 
       <div className="check_lb">
         <label>Include Lowercase Letters</label>
-          <input
-            defaultChecked={includeLowerCase}
-            type="checkbox"
-            onChange={(e) => setIncludeLowerCase(e.target.checked)}
-          />
+        <input
+          defaultChecked={includeLowerCase}
+          type="checkbox"
+          onChange={(e) => setIncludeLowerCase(e.target.checked)}
+        />
       </div>
 
       <div className="check_lb">
         <label>Include Numbers</label>
-          <input
-            defaultChecked={includeNumbers}
-            type="checkbox"
-            onChange={(e) => setIncludeNumbers(e.target.checked)}
-          />
+        <input
+          defaultChecked={includeNumbers}
+          type="checkbox"
+          onChange={(e) => setIncludeNumbers(e.target.checked)}
+        />
       </div>
 
       <div className="check_lb">
         <label>Include Symbols</label>
-          <input
-            defaultChecked={includeSymbols}
-            type="checkbox"
-            onChange={(e) => setIncludeSymbols(e.target.checked)}
-          />
+        <input
+          defaultChecked={includeSymbols}
+          type="checkbox"
+          onChange={(e) => setIncludeSymbols(e.target.checked)}
+        />
       </div>
       <div>
         <button onClick={generatePassword}>GENERATE PASSWORD</button>
